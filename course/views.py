1e1e267bfd07594779b62a54a6221ec5bf10dddf
Ikechukwu Njoku <ikthegenius@yahoo.com>
2016-09-30 20:24:06 +0100

    separate css AND small fixes, third commit

--- course/views.py
+++ course/views.py
@@ -15,18 +15,20 @@ def subchapter_list(request, pk):
 def subsection_list(request, pk, slug):
 	sub_chapter = get_object_or_404(SubChapter, slug=slug)
 	sub_sections = sub_chapter.subsection_set.all()
-	subchapter = get_object_or_404(Chapter, pk=pk)
-	return render(request, 'cefs/sub_section_list.html', {'sub_sections':sub_sections, 'subchapter':subchapter, 'sub_chapter':sub_chapter})
+	chapter = get_object_or_404(Chapter, pk=pk)
+	return render(request, 'cefs/sub_section_list.html', {'sub_sections':sub_sections, 'chapter':chapter, 'sub_chapter':sub_chapter})
 
 # edit this to determine how subsections are queried and published
 def detail(request, pk, slug, slug2):
-	course = get_object_or_404(SubSection, slug=slug2)
+	chapter = get_object_or_404(Chapter, pk=pk)
 	subsection = get_object_or_404(SubChapter, slug=slug)
+	course = get_object_or_404(SubSection, slug=slug2)
 	course_id = course.pk
 	next_course_pk = course_id + 1
 	next_course = get_object_or_404(SubSection, pk=next_course_pk)
 	context = {
 	'course':course,
+	'chapter':chapter,
 	'subsection':subsection,
 	'next_course': next_course,
 	}
